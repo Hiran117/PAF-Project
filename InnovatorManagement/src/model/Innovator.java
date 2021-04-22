aab8bd3770391da94c89ada80c50d0b0ab800c28
Dell <Dell@DESKTOP-FMUFJ65>
2021-04-22 20:42:57 +0530

    create database connection

--- InnovatorManagement/src/model/Innovator.java
+++ InnovatorManagement/src/model/Innovator.java
@@ -1,6 +1,27 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Innovator {
+	
+	//A common method to connect to the DB
+	private Connection connect() 
+	 { 
+	 Connection con = null; 
+	 try
+	 { 
+	 Class.forName("com.mysql.jdbc.Driver"); 
+	 
+	 //Provide the correct details: DBServer/DBName, username, password 
+	 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "", ""); 
+	 } 
+	 catch (Exception e) 
+	 {e.printStackTrace();} 
+	 return con; 
+	 } 
+	
+	
 	
 	
 
