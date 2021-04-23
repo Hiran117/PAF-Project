471ac8ea033e36c3f61b62224dad40f2209984b2
Hiran117 <hiranhansaka60@gmail.com>
2021-04-24 02:14:19 +0530

    add insert part

--- Fund_Management/src/com/FundingService.java
+++ Fund_Management/src/com/FundingService.java
@@ -27,6 +27,16 @@ public class FundingService {
 		return FundObj.readFunders();
 		}
 		
+		//Insertion
+		@POST
+		@Path("/")
+		@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+		@Produces(MediaType.TEXT_PLAIN)
 		
+		public String insertFunder(@FormParam("fundAmount") String fundAmount, @FormParam("name") String name, @FormParam("email") String email, @FormParam("cardNumber") String cardNumber, @FormParam("fundReqDate") String fundReqDate)
+		{
+			String output = FundObj.insertFunder(fundAmount, name, email, cardNumber, fundReqDate);
+			return output;
+		}
 	
 }
