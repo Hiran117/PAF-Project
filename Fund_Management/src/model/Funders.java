1ace1860583d8e470b5889cec548a46368cf43ea
Hiran117 <hiranhansaka60@gmail.com>
2021-04-24 01:53:21 +0530

    add retive function

--- Fund_Management/src/model/Funders.java
+++ Fund_Management/src/model/Funders.java
@@ -56,4 +56,52 @@ public class Funders {
 		}
 		return output;
 	}
+	
+	public String readFunders() {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for reading.";
+			}
+			// Prepare the html table to be displayed
+			output = "<table border=\"1\"><tr><th>Funder ID</th><th>Fund Amount</th><th>Name</th><th>Email</th><th>Card Number</th><th>Fund Request Date</th><th>Update</th><th>Remove</th></tr>";
+			String query = "select * from funders";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			// iterate through the rows in the result set
+			while (rs.next()) {
+
+				String funderID = Integer.toString(rs.getInt("funderID"));
+				String fundAmount = rs.getString("fundAmount");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				String cardNumber = rs.getString("cardNumber");
+				String fundReqDate = rs.getString("fundReqDate");
+
+				// Add into the html table
+
+				output += "<tr><td><input id='hidFunderIDUpdate' name='hidFunderIDSUpdate' type='hidden' value='"
+						+ funderID + "'>" + funderID + "</td>";
+				output += "<td>" + fundAmount + "</td>";
+				output += "<td>" + name + "</td>";
+				output += "<td>" + email + "</td>";
+				output += "<td>" + cardNumber + "</td>";
+				output += "<td>" + fundReqDate + "</td>";
+
+				// buttons
+
+				output += "<td><input name='btnUpdate' type='button' value='Update' class='btnUpdate btn btn-secondary'></td>"
+						+ "<td><input name='btnRemove' type='button' value='Remove' class='btnRemove btn btn-danger' data-funderID='"
+						+ funderID + "'>" + "</td></tr>";
+			}
+			con.close();
+			// Complete the html table
+			output += "</table>";
+		} catch (Exception e) {
+			output = "Error while reading the funders.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
 }
