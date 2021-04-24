fe2a78b81163470a6e591a957706c6289188e909
AmashaPalpita <amashapalpita@yahoo.com>
2021-04-24 19:44:41 +0530

    add insert user part

--- Users_Managment/src/model/Users.java
+++ Users_Managment/src/model/Users.java
@@ -19,4 +19,36 @@ public class Users {
 					return con;
 				}
 	
+				public String insertUser(String name, String email, String userType, String pw) {
+					String output = "";
+
+					try {
+						Connection con = connect();
+
+						if (con == null) {
+							return "Error while connecting to the database for inserting.";
+						}
+						// create a prepared statement
+						String query = "insert into users (userID,name,email,userType,pw)" + "values (?,?,?,?,?)";
+						PreparedStatement preparedStmt = con.prepareStatement(query);
+
+						// binding values
+						preparedStmt.setInt(1, 0);
+						preparedStmt.setString(2, name);
+						preparedStmt.setString(3, email);
+						preparedStmt.setString(4, userType);
+						preparedStmt.setString(5, pw);
+						// execute the statement
+						preparedStmt.execute();
+						con.close();
+
+						output = "Inserted successfully";
+
+					} catch (Exception e) {
+						output = "Error while inserting the Users.";
+						System.err.println(e.getMessage());
+					}
+					return output;
+				}
+				
 }
