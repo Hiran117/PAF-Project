a11f35507fadda412e460beec1008f2bad58c541
AmashaPalpita <amashapalpita@yahoo.com>
2021-04-24 19:50:16 +0530

    import Services

--- Users_Managment/src/com/UserService.java
+++ Users_Managment/src/com/UserService.java
@@ -1,5 +1,18 @@
 package com;
 import model.Users;
+
+//For REST Service
+	import javax.ws.rs.*;
+	import javax.ws.rs.core.MediaType;
+
+	//For JSON
+	import com.google.gson.*;
+
+	//For XML
+	import org.jsoup.*;
+	import org.jsoup.parser.*;
+	import org.jsoup.nodes.Document;
+
 public class UserService {
 
 }
